6fa9e0be0682e5d736ec2afd334fc6a3c9a6ea6d
hanan <hananjohn111@gmail.com>
2024-06-12 19:14:53 +0500

    shifted from compose to views

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.share_a_file"
+    namespace = "com.example.test"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.share_a_file"
+        applicationId = "com.example.test"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -16,7 +16,9 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
+    buildFeatures{
+        viewBinding = true
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -26,9 +28,6 @@ android {
             )
         }
     }
-    buildFeatures{
-        compose = true
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,27 +35,16 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.2"
-    }
 }
 
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-
 }
--- app/src/main/java/com/example/share_a_file/MainActivity.kt
+++ app/src/main/java/com/example/share_a_file/MainActivity.kt
@@ -1,42 +1,90 @@
 package com.example.share_a_file
 
+import android.content.Context
+import android.content.IntentFilter
+import android.net.wifi.WpsInfo
+import android.net.wifi.p2p.WifiP2pConfig
+import android.net.wifi.p2p.WifiP2pDevice
+import android.net.wifi.p2p.WifiP2pManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.share_a_file.network.manager.DownloadManager
-import com.example.share_a_file.ui.theme.Share_a_fileTheme
+import com.example.share_a_file.receiver.WiFiBroadcastReceiver
+import com.example.test.databinding.ActivityMainLayoutBinding
 
 class MainActivity : ComponentActivity() {
 
+    private lateinit var binding:ActivityMainLayoutBinding
     lateinit var dm : DownloadManager
+    lateinit var receiver:WiFiBroadcastReceiver
+    private val intentFilter = IntentFilter()
+    private lateinit var channel: WifiP2pManager.Channel
+    private lateinit var manager: WifiP2pManager
+
+    var isWifiP2pEnabled:Boolean = false
+
+    private val peers = mutableListOf<WifiP2pDevice>()
+
+    val peerListListener = WifiP2pManager.PeerListListener { peerList ->
+        val refreshedPeers = peerList.deviceList
+        if (refreshedPeers != peers) {
+            peers.clear()
+            peers.addAll(refreshedPeers)
+
+            // If an AdapterView is backed by this data, notify it
+            // of the change. For instance, if you have a ListView of
+            // available peers, trigger an update.
+//            (listAdapter as WiFiPeerListAdapter).notifyDataSetChanged()
+
+            // Perform any other updates needed based on the new list of
+            // peers connected to the Wi-Fi P2P network.
+        }
+
+        if (peers.isEmpty()) {
+            Log.d("TAG", "No devices found")
+            return@PeerListListener
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Share_a_fileTheme{
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) {innerPaddings->
-                    HomeScreen(
-                        Modifier.padding(innerPaddings)
-                    )
+        binding = ActivityMainLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-                }
+        // Indicates a change in the Wi-Fi Direct status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+
+        // Indicates the state of Wi-Fi Direct connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+
+        manager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
+        channel = manager.initialize(this, mainLooper, null)
+
+        manager.discoverPeers(channel, object : WifiP2pManager.ActionListener {
+
+            override fun onSuccess() {
+                // Code for when the discovery initiation is successful goes here.
+                // No services have actually been discovered yet, so this method
+                // can often be left blank. Code for peer discovery goes in the
+                // onReceive method, detailed below.
             }
-        }
+
+            override fun onFailure(reasonCode: Int) {
+                // Code for when the discovery initiation fails goes here.
+                // Alert the user that something went wrong.
+            }
+        })
+
+
+
 
 //        button.setOnClickListener {
 //            ConnectionStateListener.getConnectionState(this)
@@ -57,37 +105,40 @@ class MainActivity : ComponentActivity() {
 //        }
     }
 
-}
-
-@Composable
-fun HomeScreen(modifier: Modifier) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
-            .fillMaxSize()
-    ) {
-         Button(
-                onClick = {
-                    
-                }) {
-                Text(text = "Connect")
-         }
-        Button(
-                onClick = { /*TODO*/ }) {
-                Text(text = "Select")
-        }
-        Button(
-            onClick = { /*TODO*/ }) {
-            Text(text = "Send")
-        }
+    override fun onResume() {
+        super.onResume()
+        receiver = WiFiBroadcastReceiver(manager, channel, this)
+        registerReceiver(receiver, intentFilter)
     }
-}
 
-@Composable
-@Preview(showBackground = true)
-fun HomePreview(){
-    Share_a_fileTheme {
-        HomeScreen(Modifier.fillMaxSize())
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(receiver)
+    }
+
+    fun connectToPeer(){
+        // Picking the first device found on the network.
+        val device = peers[0]
+
+        val config = WifiP2pConfig().apply {
+            deviceAddress = device.deviceAddress
+            wps.setup = WpsInfo.PBC
+        }
+
+        manager.connect(channel, config, object : WifiP2pManager.ActionListener {
+
+            override fun onSuccess() {
+                // WiFiDirectBroadcastReceiver notifies us. Ignore for now.
+            }
+
+            override fun onFailure(reason: Int) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Connect failed. Retry.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+
     }
 }
